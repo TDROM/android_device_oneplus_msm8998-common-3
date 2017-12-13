285396bc3566a4968ee7d0bf4798e10136e227c9
Yifan Hong <elsk@google.com>
2017-12-13 03:06:00 +0100

    msm8998: track carrbon file and fix recovery
    Change-Id: I0f5689344bb7b10b2b5db2549e95c940c4812f9d
    Signed-off-by: Mrinal Kant Shukla <kamikaze.mrinal@gmail.com>

--- recovery/Android.mk
+++ recovery/Android.mk
@@ -7,9 +7,9 @@ LOCAL_CFLAGS += -DUSES_BOOTDEVICE_PATH
 endif
 
 LOCAL_C_INCLUDES := \
-    $(TARGET_OUT_HEADERS)/edify \
-    $(TARGET_OUT_HEADERS)/otautil \
-    $(TARGET_OUT_HEADERS)/updater
+    $(call project-path-for,recovery)/edify/include \
+    $(call project-path-for,recovery)/otautil/include \
+    $(call project-path-for,recovery)/updater/include
 
 LOCAL_SRC_FILES := recovery_updater.cpp
 LOCAL_MODULE := librecovery_updater_oneplus
