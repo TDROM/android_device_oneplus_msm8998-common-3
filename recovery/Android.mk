0469f7cea2b0c4350a9822ece7fa823e48eaedb0
kazerine <kamikaze.mrinal@gmail.com>
2018-09-03 16:41:12 +0000

    msm8998: fix correct path for recovery

--- recovery/Android.mk
+++ recovery/Android.mk
@@ -7,9 +7,9 @@ LOCAL_CFLAGS += -DUSES_BOOTDEVICE_PATH
 endif
 
 LOCAL_C_INCLUDES := \
-    $(TARGET_OUT_HEADERS)/edify \
-    $(TARGET_OUT_HEADERS)/otautil \
-    $(TARGET_OUT_HEADERS)/updater
+    bootable/recovery/edify/include \
+    bootable/recovery/otautil/include \
+    bootable/recovery/updater/include
 
 LOCAL_SRC_FILES := recovery_updater.cpp
 LOCAL_MODULE := librecovery_updater_oneplus
